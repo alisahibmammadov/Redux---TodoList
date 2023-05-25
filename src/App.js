f49ace136264fabc6979c0b5327e681bbe6c9a6e
alisahib <alisahibmammadov98@gmail.com>
2023-05-25 11:44:37 -0700

    Redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { AddTodoAction, RemoveTodoAction } from "./actions/TodoActions";
 
 function App() {
+  const [todo, setTodo] = useState();
+  const dispatch = useDispatch();
+  const Todo = useSelector((state) => state.Todo);
+
+  const { todos } = Todo;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (todo) {
+      dispatch(AddTodoAction(todo));
+    
+    }
+  };
+  const removeHandler = (e) => {
+    dispatch(RemoveTodoAction(e));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2> Redux ile Todo List</h2>
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            placeholder="Todo daxil et"
+            style={{
+              width: 350,
+              padding: 10,
+              borderRadius: 20,
+              border: "none",
+              fontSize: 20,
+            }}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button
+            type="submit"
+            style={{
+              padding: 12,
+              borderRadius: 25,
+              fontSize: 15,
+              marginLeft: 20,
+              cursor:'pointer'
+            }}
+            onKeyPress={handleSubmit}
+          >
+            Göndər
+          </button>
+        </form>
+        <ul className="allTodos">
+          {todos &&
+            todos.map((t) => (
+              <li key={t.id} className="singleTodo ">
+                <span className="todoText">{t.todo}</span>
+                <button
+                  
+                  onClick={() => removeHandler(t)}
+                >
+                  Sil
+                </button>
+              </li>
+            ))}
+        </ul>
       </header>
     </div>
   );
